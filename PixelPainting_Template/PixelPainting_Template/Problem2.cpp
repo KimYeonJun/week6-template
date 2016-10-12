8819984b8cf29ac03425454a0127fcf18c240db9
duswn <duswn@DESKTOP-PUMBLSG>
2016-10-12 13:45:55 +0900

    week6-template.

--- PixelPainting_Template/PixelPainting_Template/Problem2.cpp
+++ PixelPainting_Template/PixelPainting_Template/Problem2.cpp
@@ -1,30 +1,30 @@
-//#include <iostream>
-//
-//template<class TT>
-//class Vector2D
-//{
-//public:
-//	TT x_, y_;
-//};
-//template<class T_CLASS>
-//void print(const T_CLASS& i)
-//{
-//	std::cout << i << std::endl;
-//
-//}
-//int main()
-//{
-//	Vector2D<float> my_vector;
-//	my_vector.x_ = 3.5f;
-//	my_vector.y_ = 4.5f;
-//	print(my_vector.x_ + my_vector.y_);
-//	Vector2D<int> my_vector2;
-//	my_vector2.x_ = 3;
-//	my_vector2.y_ = 4;
-//	print(my_vector2.x_ + my_vector2.y_);
-//	Vector2D<double> my_vector3;
-//	my_vector3.x_ = 4.5;
-//	my_vector3.y_ = 6.4;
-//	print(my_vector3.x_ + my_vector3.y_);
-//	return 0;
-//}
+#include <iostream>
+
+template<class TT>
+class Vector2D
+{
+public:
+	TT x_, y_;
+};
+template<class T_CLASS>
+void print(const T_CLASS& i)
+{
+	std::cout << i << std::endl;
+
+}
+int main()
+{
+	Vector2D<float> my_vector;
+	my_vector.x_ = 3.5f;
+	my_vector.y_ = 4.5f;
+	print(my_vector.x_ + my_vector.y_);
+	Vector2D<int> my_vector2;
+	my_vector2.x_ = 3;
+	my_vector2.y_ = 4;
+	print(my_vector2.x_ + my_vector2.y_);
+	Vector2D<double> my_vector3;
+	my_vector3.x_ = 4.5;
+	my_vector3.y_ = 6.4;
+	print(my_vector3.x_ + my_vector3.y_);
+	return 0;
+}
